2d52fc898af1f9b105fbc6d0702da2a0e6745d44
Maryam Abdi <maryamao@ucar.edu>
2020-07-24 16:47:41 -0600

    add CodeCov to CodeBuild gnu (#412)
    * try debug first
    
    * bugfix
    
    * use RelWithDebInfo
    
    * debug
    
    * ok codecov works now run it
    
    * add codecov to readme
    
    * bugfix
    
    * add -R and run fewer test for debugging
    
    * cd to build dir
    
    * run all tests!

--- CI/codecov_script.sh
+++ CI/codecov_script.sh
@@ -1,2 +1,2 @@
-#/bin/bash	
-bash <(curl -s https://codecov.io/bash) -t 845d92f2-adff-428a-b208-770e6e23e478
+#/bin/bash
+bash <(curl -s https://codecov.io/bash) -t 845d92f2-adff-428a-b208-770e6e23e478 -R /jcsda/fv3-bundle/fv3-jedi
