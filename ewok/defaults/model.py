c1948595dadb5e187a327d7871e338e8af135606
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-10-07 16:21:57 -0400

    Feature/fv3fcst (#509)
    * fv3forecasts are working now
    
    * change output frequency to 1H. Add execcmd to fcout.yaml.  It should be elsewhere
    
    * fv3forecasts are working now
    
    * change output frequency to 1H. Add execcmd to fcout.yaml.  It should be elsewhere
    
    Co-authored-by: danholdaway <danholdaway@users.noreply.github.com>

--- ewok/defaults/model.py
+++ ewok/defaults/model.py
@@ -1,6 +1,8 @@
 import os
 import datetime
 import ewok
+import solo
+
 
 __all__ = ["fc_file", "obs_file", "r2d2_obsfile", "r2d2_anfile", "r2d2_fixfile"]
 
@@ -27,19 +29,19 @@ def r2d2_obsfile(conf, date):
 
 
 def _get_restarts(conf, date):
-    dt_step = ewok.parse_timedelta(conf['step_cycle'])
-    bdate = date - dt_step / 2
-    rdate = bdate.strftime('%Y%m%d.%H')
+    # rdate = date.strftime('%Y%m%d.%H')
 
     ntiles = conf.get('ntiles', 6)
 
     ftypes = conf.get('ftypes', ['fv_core.res', 'fv_srf_wnd.res', 'fv_tracer.res', 'sfc_data'])
-    ftiles = [f'tile{t+1}' for t in range(0, conf.get('ntiles', 6)]
+    ftiles = [f'tile{t+1}' for t in range(0, ntiles)]
 
     restarts = []
+    fname = 'coupler.res'
+    restarts.append(fname)
     for ftype in ftypes:
         for tile in ftiles:
-            #fname = f'{rdate}0000.{ftype}.{tile}.nc'
+            # fname = f'{rdate}0000.{ftype}.{tile}.nc'
             fname = f'{ftype}.{tile}.nc'
             restarts.append(fname)
     return restarts
@@ -50,7 +52,7 @@ def _get_fv3files(conf):
     npz = conf.get('npz', 64)
     res = conf.get('resolution', 'c12')
 
-    flist = conf.get(fv3filesList, [f'akbk{npz}.nc4', 'field_table', 'fmsmpp.nml', f'input_gfs_{res}.nml', 'inputpert_4dvar.nml'])
+    flist = conf.get('fv3filesList', [f'akbk{npz}.nc4', 'field_table', 'fmsmpp.nml', f'input_gfs_{res}.nml', 'inputpert_4dvar.nml'])
 
     fv3files = []
     for fname in flist:
@@ -61,7 +63,7 @@ def _get_fv3files(conf):
 
 def _get_fieldsets(conf):
 
-    flist = conf.get(fieldsetsList, ['dynamics.yaml'])
+    flist = conf.get('fieldsetsList', ['dynamics.yaml'])
 
     fieldsets = []
     for fname in flist:
@@ -74,11 +76,11 @@ def r2d2_anfile(conf, date):
 
     anlfileDict = {}
     restarts = _get_restarts(conf, date)
-    fv3files = _get_fieldsets(conf)
+    fv3files = _get_fv3files(conf)
     fieldsets = _get_fieldsets(conf)
 
-    anlfileDict = ['restarts' : restarts,
+    anlfileDict = {'restarts' : restarts,
                    'fv3files' : fv3files,
-                   'fieldsets': fieldsets]
+                   'fieldsets': fieldsets}
 
     return anlfileDict
