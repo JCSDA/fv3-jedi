cf81b2c73f4640aceabc466eb92c32de53ef42d3
Dan Holdaway <27729500+danholdaway@users.noreply.github.com>
2022-05-10 12:58:46 -0400

    Remove IO from Analysis2Model variable change (#470)
    Co-authored-by: danholdaway <danholdaway@users.noreply.github.com>

--- src/fv3jedi/VariableChange/Analysis2Model/fv3jedi_varcha_a2m_mod.f90
+++ src/fv3jedi/VariableChange/Analysis2Model/fv3jedi_varcha_a2m_mod.f90
@@ -18,8 +18,6 @@ use datetime_mod
 use fv3jedi_kinds_mod,                  only: kind_real
 use fv3jedi_geom_mod,                   only: fv3jedi_geom
 use fv3jedi_state_mod,                  only: fv3jedi_state
-use fv3jedi_io_fms_mod,                 only: fv3jedi_io_fms
-use fv3jedi_io_cube_sphere_history_mod, only: fv3jedi_io_cube_sphere_history
 use fv3jedi_field_mod,                  only: copy_subset
 use wind_vt_mod,                        only: a_to_d, d_to_a
 
@@ -27,9 +25,6 @@ implicit none
 private
 public :: fv3jedi_varcha_a2m
 type :: fv3jedi_varcha_a2m
-  character(len=96)     :: filetype !IO type
-  type(fv3jedi_io_fms)  :: fms
-  type(fv3jedi_io_cube_sphere_history) :: csh
   contains
     procedure :: create
     procedure :: delete
@@ -45,26 +40,11 @@ contains
 
 subroutine create(self, geom, conf)
 
-implicit none
 class(fv3jedi_varcha_a2m), intent(inout) :: self
 type(fv3jedi_geom),        intent(inout) :: geom
 type(fckit_configuration), intent(in)    :: conf
 
-character(len=:), allocatable :: str
-
-! Get IO type to use
-call conf%get_or_die("filetype", str)
-self%filetype = str
-
-! Setup IO from config
-if (trim(self%filetype) == 'fms restart') then
-  call self%fms%create(conf, geom%domain, geom%npz)
-elseif (trim(self%filetype) == 'cube sphere history') then
-  call self%csh%create(geom, conf)
-else
-  call abor1_ftn("fv3jedi_varcha_a2m_mod: filetype must be cube sphere history or fms restart, is "// &
-                 trim(self%filetype)//".")
-endif
+! Nothing to do
 
 end subroutine create
 
@@ -72,11 +52,9 @@ end subroutine create
 
 subroutine delete(self)
 
-implicit none
 class(fv3jedi_varcha_a2m), intent(inout) :: self
 
-if (trim(self%filetype) == 'fms restart') call self%fms%delete()
-if (trim(self%filetype) == 'cube sphere history') call self%csh%delete()
+! Nothing to do
 
 end subroutine delete
 
@@ -84,7 +62,6 @@ end subroutine delete
 
 subroutine changevar(self,geom,xana,xmod)
 
-implicit none
 class(fv3jedi_varcha_a2m), intent(inout) :: self
 type(fv3jedi_geom),        intent(inout) :: geom
 type(fv3jedi_state),       intent(in)    :: xana
@@ -120,9 +97,6 @@ do index_mod = 1, xmod%nf
     !OK, direct copy
     xmod%fields(index_mod)%array = xana%fields(index_ana_found)%array
     failed = .false.
-    if (xmod%f_comm%rank() == 0) write(*,"(A, A10, A, A10)") &
-        "A2M ChangeVar: analysis state "//xana%fields(index_ana_found)%fv3jedi_name(1:10)&
-        //" => model state "//xmod%fields(index_mod)%fv3jedi_name(1:10)
 
   elseif (xmod%fields(index_mod)%fv3jedi_name == 'ud') then
 
@@ -136,8 +110,6 @@ do index_mod = 1, xmod%nf
       xmod_ud(:,geom%jec+1,:) = 0.0_kind_real
       xmod_vd(geom%iec+1,:,:) = 0.0_kind_real
       failed = .false.
-      if (xmod%f_comm%rank() == 0) write(*,"(A)") &
-          "A2M ChangeVar: analysis state ua         => model state ud"
     endif
 
   elseif (xmod%fields(index_mod)%fv3jedi_name == 'vd') then
@@ -146,8 +118,6 @@ do index_mod = 1, xmod%nf
     if (xana%has_field('ua')) then
       !Already done above
       failed = .false.
-      if (xmod%f_comm%rank() == 0) write(*,"(A)") &
-          "A2M ChangeVar: analysis state va         => model state vd"
     endif
 
   elseif (xmod%fields(index_mod)%fv3jedi_name == 'delp') then
@@ -160,25 +130,14 @@ do index_mod = 1, xmod%nf
         xmod_delp(:,:,k) = (geom%ak(k+1)-geom%ak(k)) + (geom%bk(k+1)-geom%bk(k))*xana_ps(:,:,1)
       enddo
       failed = .false.
-      if (xmod%f_comm%rank() == 0) write(*,"(A)") &
-          "A2M ChangeVar: analysis state ps         => model state delp"
     endif
 
   endif
 
-  if (failed) then
-
-    if (xmod%f_comm%rank() == 0) write(*,"(A)") &
-        "Found no way of getting "//trim(xmod%fields(index_mod)%fv3jedi_name)//" from the analysis state."//&
-        "Attempting to read from file"
-
-    if (trim(self%filetype) == 'fms restart') then
-      call self%fms%read(xmod%time, xmod%fields(index_mod:index_mod))
-    elseif (trim(self%filetype) == 'cube sphere history') then
-      call self%csh%read(xmod%time, xmod%fields(index_mod:index_mod))
-    endif
-
-  endif
+  if (failed) &
+    call abor1_ftn("fv3jedi_linvarcha_a2m_mod.changevar: Found no way of getting " &
+                   // trim(xmod%fields(index_mod)%fv3jedi_name) // &
+                   " from the analysis state.")
 
 enddo
 
@@ -188,7 +147,6 @@ end subroutine changevar
 
 subroutine changevarinverse(self,geom,xmod,xana)
 
-implicit none
 class(fv3jedi_varcha_a2m), intent(inout) :: self
 type(fv3jedi_geom),        intent(inout) :: geom
 type(fv3jedi_state),       intent(in)    :: xmod
@@ -223,9 +181,6 @@ do index_ana = 1, xana%nf
     !OK, direct copy
     failed = .false.
     xana%fields(index_ana)%array = xmod%fields(index_mod_found)%array
-    if (xana%f_comm%rank() == 0) write(*,"(A, A10, A, A10)") &
-        "A2M ChangeVarInverse: model state "//xmod%fields(index_mod_found)%fv3jedi_name(1:10)&
-        //" => analysis state "//xana%fields(index_ana)%fv3jedi_name(1:10)
 
   elseif (xana%fields(index_ana)%fv3jedi_name == 'ua') then
 
@@ -239,8 +194,6 @@ do index_ana = 1, xana%nf
       xmod_vd(geom%iec+1,:,:) = 0.0_kind_real
       call d_to_a(geom, xmod_ud, xmod_vd, xana_ua, xana_va)
       failed = .false.
-      if (xana%f_comm%rank() == 0) write(*,"(A)") &
-          "A2M ChangeVarInverse: model state ud         => analysis state ua"
     endif
 
   elseif (xana%fields(index_ana)%fv3jedi_name == 'va') then
@@ -249,8 +202,6 @@ do index_ana = 1, xana%nf
     if (xmod%has_field('ud')) then
       !Already done above
       failed = .false.
-      if (xana%f_comm%rank() == 0) write(*,"(A)") &
-          "A2M ChangeVarInverse: model state vd         => analysis state va"
     endif
 
   elseif (xana%fields(index_ana)%fv3jedi_name == 'ps') then
@@ -261,14 +212,14 @@ do index_ana = 1, xana%nf
       call xmod%get_field('delp', xmod_delp)
       xana_ps(:,:,1) = sum(xmod_delp,3)
       failed = .false.
-      if (xana%f_comm%rank() == 0) write(*,"(A)") &
-          "A2M ChangeVarInverse: model state delp       => analysis state ps"
     endif
 
   endif
 
-  if (failed) call abor1_ftn("fv3jedi_linvarcha_a2m_mod.changevarinverse: found no way of getting "//&
-                             trim(xana%fields(index_ana)%fv3jedi_name)//" from the model state" )
+  if (failed) &
+    call abor1_ftn("fv3jedi_linvarcha_a2m_mod.changevarinverse: Found no way of getting " &
+                   // trim(xmod%fields(index_mod)%fv3jedi_name) // &
+                   " from the model state.")
 
 enddo
 
