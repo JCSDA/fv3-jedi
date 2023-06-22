77d22911ebc40e618935b2bc0c9d7815fa22d137
Benjamin Menetrier <30638301+benjaminmenetrier@users.noreply.github.com>
2023-06-22 17:42:26 +0200

    Merge B matrix-related applications (#909)
    * Update executable and yamls
    
    * Trigger tests
    
    ---------
    
    Co-authored-by: Benjamin Menetrier <benjamin.menetrier@irit.fr>
    Co-authored-by: Anna Shlyaeva <annash@ucar.edu>

--- src/mains/fv3jediErrorCovarianceToolbox.cc
+++ src/mains/fv3jediErrorCovarianceToolbox.cc
@@ -1,18 +1,18 @@
 /*
- * (C) Copyright 2017-2020 UCAR
+ * (C) Copyright 2018-2020 UCAR.
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
 #include "fv3jedi/Utilities/Traits.h"
-#include "oops/runs/Dirac.h"
 #include "oops/runs/Run.h"
+#include "saber/oops/ErrorCovarianceToolbox.h"
 #include "saber/oops/instantiateCovarFactory.h"
 
 int main(int argc,  char ** argv) {
   oops::Run run(argc, argv);
   saber::instantiateCovarFactory<fv3jedi::Traits>();
-  oops::Dirac<fv3jedi::Traits> dir;
-  return run.execute(dir);
+  saber::ErrorCovarianceToolbox<fv3jedi::Traits> ectb;
+  return run.execute(ectb);
 }
