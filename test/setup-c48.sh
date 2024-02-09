2061f56ed0a4cb45ca6cf3a78a444b41f3b4090e
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-02-09 11:48:01 -0700

    Bug fix in test/setup-c48.sh: detect macOS and use gsed if found, otherwise abort (#1132)

--- test/setup-c48.sh
+++ test/setup-c48.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # this script sets up inputs and sets dummy variables so that the first part of the ufs-weather-model run_test.py script will
 # work on a generic system and generate the experiment directory that is used to run the ufs forecast regression tests.
 set -x
@@ -52,8 +52,15 @@ touch fv3_001.exe
 touch modules.fv3_001.lua
 source vars
 
+if [ "$(uname)" == "Darwin" ]; then
+  hash gsed 2>/dev/null || { echo >&2 "GNU SED (gsed) required on macOS, but not installed. Aborting."; exit 1; }
+  SED=gsed
+else
+  SED=sed
+fi
+
 # short circuit the job submission here. We only care about getting the input data set up
-sed -i "/submit_and_wait/a exit 0" rt_utils.sh 
+${SED} -i "/submit_and_wait/a exit 0" rt_utils.sh
 
 # Generate the control_c48_intel regression test experiment directory for a cold start
 ./run_test.sh $PWD $fv3jedidata control_c48 001 001
@@ -64,28 +71,28 @@ cd $fv3jedidata/control_c48_intel
 cp $INPUTDATA_ROOT/RESTART/* INPUT
 cd INPUT
 for file in 20210323.060000.*; do new=$(echo $file | cut -c 17-) && mv -v -- "$file" "$new" ; done
-sed -i 's/3    22/3    23/g' coupler.res 
+${SED} -i 's/3    22/3    23/g' coupler.res
 cd ..
 
 # update model_configure file to turn off write component and do a warm start for our regression test
-sed -i 's/quilting:                .true./quilting:                .false./g' model_configure 
-sed -i 's/start_day:               22/start_day:               23/g' model_configure 
-sed -i '/quilting_restart/d' model_configure 
+${SED} -i 's/quilting:                .true./quilting:                .false./g' model_configure
+${SED} -i 's/start_day:               22/start_day:               23/g' model_configure
+${SED} -i '/quilting_restart/d' model_configure
 
 #change ufs.configure to use only 6 cores (no write component)
-sed -i 's/0 7/0 5/g' ufs.configure
-sed -i '/ATM_omp/d' ufs.configure
+${SED} -i 's/0 7/0 5/g' ufs.configure
+${SED} -i '/ATM_omp/d' ufs.configure
 
 #switch to warm_start and turn off checksum comparisons
-sed -i 's/warm_start = .false./warm_start = .true./g' input.nml
-sed -i 's/make_nh = .true./make_nh = .false./g' input.nml
-sed -i 's/na_init = 1/na_init = 0/g' input.nml
-sed -i 's/external_ic = .true./external_ic = .false./g' input.nml
-sed -i 's/nggps_ic = .true./nggps_ic = .false./g' input.nml
-sed -i 's/mountain = .false./mountain = .true./g' input.nml
-sed -i '/atmos_model_nml/a\  ignore_rst_cksum = .true.' input.nml
-sed -i '/fv_core_nml/a\  ignore_rst_cksum = .true.' input.nml
-sed -i '/&fms_nml/i \
+${SED} -i 's/warm_start = .false./warm_start = .true./g' input.nml
+${SED} -i 's/make_nh = .true./make_nh = .false./g' input.nml
+${SED} -i 's/na_init = 1/na_init = 0/g' input.nml
+${SED} -i 's/external_ic = .true./external_ic = .false./g' input.nml
+${SED} -i 's/nggps_ic = .true./nggps_ic = .false./g' input.nml
+${SED} -i 's/mountain = .false./mountain = .true./g' input.nml
+${SED} -i '/atmos_model_nml/a\  ignore_rst_cksum = .true.' input.nml
+${SED} -i '/fv_core_nml/a\  ignore_rst_cksum = .true.' input.nml
+${SED} -i '/&fms_nml/i \
 &fms_io_nml\
   checksum_required = .false.\
 /' input.nml
