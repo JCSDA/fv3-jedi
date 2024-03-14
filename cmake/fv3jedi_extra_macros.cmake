95508eb89cfb76191e7eb880af1e6ee04ead0141
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-03-13 20:17:10 -0600

    Build system updates: ecbuild dependency, extra cmake macros (#1159)
    * Build system updates: ecbuild dependency, extra cmake macros
    
    * Trigger CI

--- cmake/fv3jedi_extra_macros.cmake
+++ cmake/fv3jedi_extra_macros.cmake
@@ -21,6 +21,24 @@ macro( LINK_FILES_DIR filelist dst_dir )
   endforeach(FILENAME)
 endmacro()
 
+# macro to create a symlink from src to dst
+function(CREATE_SYMLINK src dst)
+    foreach (FILENAME ${ARGN})
+        execute_process( COMMAND ${CMAKE_COMMAND} -E create_symlink
+            ${src}/${FILENAME}
+            ${dst}/${FILENAME} )
+        endforeach(FILENAME)
+endfunction(CREATE_SYMLINK)
+
+# macro to create a symlink from src to dst with just filename
+function(CREATE_SYMLINK_FILENAME src dst)
+    foreach (FILENAME ${ARGN})
+        get_filename_component(filename ${FILENAME} NAME )
+        execute_process( COMMAND ${CMAKE_COMMAND} -E create_symlink
+            ${src}/${FILENAME}
+            ${dst}/${filename} )
+        endforeach(FILENAME)
+endfunction(CREATE_SYMLINK_FILENAME)
 
 # Macro to include GFS restart files in testing
 macro( GFS_FILES_BKG path date )
