d2d51e9c4714932966239f7fe7531735eac6f349
Cory Martin <cory.r.martin@noaa.gov>
2024-04-09 19:07:05 +0000

    Companion PR to OOPS for Gaussian Grid Writer (#1160)
    * Changes for gaussian grid output
    
    * Missing some spots
    
    * fix typo
    
    * rename file
    
    * test passing
    
    * update test
    
    * update yaml
    
    * Revert "update yaml"
    
    This reverts commit 62d8c1a1ff1bd3b78a6129217df30b97aa30d00b.
    
    * fix my stupid debugging commit
    
    * fix missing line
    
    ---------
    
    Co-authored-by: Anna Shlyaeva <annash@ucar.edu>

--- src/mains/fv3jediConvertToStructuredGrid.cc
+++ src/mains/fv3jediConvertToStructuredGrid.cc
@@ -1,17 +1,17 @@
 /*
- * (C) Copyright 2023 UCAR
+ * (C) Copyright 2023-2024 UCAR
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
 #include "fv3jedi/Utilities/Traits.h"
-#include "oops/runs/ConvertToLatLon.h"
+#include "oops/runs/ConvertToStructuredGrid.h"
 #include "oops/runs/Run.h"
 
 int main(int argc,  char ** argv) {
   oops::Run run(argc, argv);
-  oops::ConvertToLatLon<fv3jedi::Traits> ctll;
+  oops::ConvertToStructuredGrid<fv3jedi::Traits> ctll;
   return run.execute(ctll);
 }
 
