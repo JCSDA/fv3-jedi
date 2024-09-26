26f516ae3aed94755db54730b0f5020f63ad6756
Steve Vahl <42982505+svahl991@users.noreply.github.com>
2024-09-26 09:18:29 -0600

    Fix error in Vader custom cookbook definition (#1269)

--- src/fv3jedi/VariableChange/VaderCookbook.h
+++ src/fv3jedi/VariableChange/VaderCookbook.h
@@ -18,9 +18,9 @@ namespace fv3jedi {
 // -------------------------------------------------------------------------------------------------
 
   static std::map<std::string, std::vector<std::string>> vaderFV3CustomCookbook() {
-    return
-      // pt: from t and pkz
-      {{"potential_temperature",        {"AirPotentialTemperature_B"}},
+    return {
+      // pt: from t and pkz, from t and ps
+      {"potential_temperature",        {"AirPotentialTemperature_B", "AirPotentialTemperature_A"}},
       // P: from delp, from ps (and ak/bk)
       {"air_pressure_levels",          {"AirPressureAtInterface_B", "AirPressureAtInterface_A"}},
       // p: from pe
@@ -31,12 +31,11 @@ namespace fv3jedi {
       {"air_pressure_to_kappa",        {"AirPressureToKappa_A"}},
       // delp: from p
       {"air_pressure_thickness",       {"AirPressureThickness_A"}},
-      // pt: from t and ps
-      {"potential_temperature",        {"AirPotentialTemperature_A"}},
       // ps: from delp
       {"surface_pressure",             {"SurfaceAirPressure_A"}},
       // tv: from t and q
-      {"virtual_temperature",          {"AirVirtualTemperature_A"}}};
+      {"virtual_temperature",          {"AirVirtualTemperature_A"}}
+    };
   }
 
 }  // namespace fv3jedi
