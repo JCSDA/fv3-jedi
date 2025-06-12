19345fc9ba30cc3873ba7a9aed6cbdac29516eb7
Cory Martin <cory.r.martin@noaa.gov>
2025-06-12 19:27:50 +0000

    add snowDensity as a fv3-jedi field (#1373)

--- src/fv3jedi/FieldMetadata/FieldsMetadataDefault.h
+++ src/fv3jedi/FieldMetadata/FieldsMetadataDefault.h
@@ -523,6 +523,14 @@ namespace fv3jedi {
     md.space = "magnitude";
     addFieldMetadata(fieldsmetadata, nlev, md);
 
+    md.longName = "snowDensity";
+    md.units = "kgm-3";
+    md.kind = "double";
+    md.tracer = "false";
+    md.levels = "1";
+    md.space = "magnitude";
+    addFieldMetadata(fieldsmetadata, nlev, md);
+
     md.longName = "eastward_wind_at_surface";
     md.units = "ms-1";
     md.kind = "double";
