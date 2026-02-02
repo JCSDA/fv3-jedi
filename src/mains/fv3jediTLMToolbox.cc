4875b0e355bc3c0ed9e919f950dee2b4942879c6
Christian Sampson <48019844+xian22@users.noreply.github.com>
2026-02-02 12:03:13 -0700

    Feature/add tlm toolbox (#1446)
    * adding executable and yaml
    
    * fix yaml add ref
    
    * update ref to match exactly tlm toolbox output, test passes with previous linearization error output
    
    * remove linearization error app and test
    
    * update sources
    
    * remove dupicate entry
    
    * Trigger CI
    
    ---------
    
    Co-authored-by: Francois Hebert <hebert@ucar.edu>

--- src/mains/fv3jediTLMToolbox.cc
+++ src/mains/fv3jediTLMToolbox.cc
@@ -1,17 +1,16 @@
 /*
- * (C) Copyright 2023 UCAR.
- * (C) Crown copyright 2023 Met Office.
+ * (C) Copyright 2025 UCAR.
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
 #include "fv3jedi/Utilities/Traits.h"
-#include "oops/runs/LinearizationError.h"
 #include "oops/runs/Run.h"
+#include "oops/runs/TLMToolbox.h"
 
 int main(int argc,  char ** argv) {
   oops::Run run(argc, argv);
-  oops::LinearizationError<fv3jedi::Traits> linearizationError;
-  return run.execute(linearizationError);
+  oops::TLMToolbox<fv3jedi::Traits> tlmToolbox;
+  return run.execute(tlmToolbox);
 }
