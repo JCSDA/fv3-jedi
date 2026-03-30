c77a0f64af68af9ef54a0f1673d6fd55488f0b53
Francois Hebert <hebert@ucar.edu>
2026-03-30 14:21:44 -0600

    Clean up compiler flag specification (#1478)
    Co-authored-by: Yannick Trémolet <30638944+ytremolet@users.noreply.github.com>

---
+++ cmake/fv3jedi_compiler_flags.cmake
@@ -0,0 +1,15 @@
+# (C) Copyright 2026 UCAR
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+
+# Set compiler flags for basic build types,
+# for compilers where this is not provided by ecbuild.
+include(build_type_compiler_flags)
+
+# Set JEDI's common compiler flags
+include(jedi_common_compiler_flags)
+
+# Set fv3-jedi-specific compiler flags
+# none
