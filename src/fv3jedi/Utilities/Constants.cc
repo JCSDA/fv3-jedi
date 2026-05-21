e06b348fb25948974cf17007e4ffd588fcf2ff3c
Clementine Hardy Gas <43183478+cmgas@users.noreply.github.com>
2026-05-21 13:42:59 -0600

    fix ref and add refpressure to constants (#1508)

--- src/fv3jedi/Utilities/Constants.cc
+++ src/fv3jedi/Utilities/Constants.cc
@@ -26,6 +26,7 @@ namespace fv3jedi {
     static const double cpdry          = 3.5*rdry;
     static const double rvap           = runiv/h2omw;
     static const double kappa          = rdry/cpdry;
+    static const double p0             = 100000.0;
     static const double epsilon        = h2omw/airmw;
     static const double zvir           = rvap/rdry - 1.;
     static const double lapse_rate     = -0.0065;
@@ -46,6 +47,7 @@ namespace fv3jedi {
         {"cpdry", cpdry},
         {"rvap", rvap},
         {"kappa", kappa},
+        {"reference_pressure", p0},
         {"epsilon", epsilon},
         {"zvir", zvir},
         {"lapse_rate", lapse_rate},
